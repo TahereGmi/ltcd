bf947e4d7bd58cde04427a4763e3bd7a619cfe4d
TahereGmi <mstgholami@gmail.com>
2023-03-10 13:19:31 +0330

    Add palindrome number - integer version

--- palindromeNumber.js
+++ palindromeNumber.js
@@ -7,3 +7,20 @@ var isPalindrome = function(x) {
     const reversedStr = reversedArr.join("")
     return xStr === reversedStr
 };
+
+// whithout transform to string
+function isPalindrome(x) {
+    if(x < 0) {
+        return false;
+    } else if(x < 10) {
+        return true;
+    } else {
+        let reverse = 0;
+        let original = x;
+        while (x > 0) {
+            reverse = reverse * 10 + x % 10;
+            x = Math.floor(x / 10)
+        }
+        return reverse === original
+    }
+}
