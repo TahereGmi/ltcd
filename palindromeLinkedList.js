df187aaa69c355dcd63a1996980ef0454e35faa0
TahereGmi <mstgholami@gmail.com>
2023-03-12 19:07:58 +0330

    Check palindrome linkedList in O(n) time and O(1) space

--- palindromeLinkedList.js
+++ palindromeLinkedList.js
@@ -32,3 +32,40 @@ function isPalindrome(head) {
     }
     return true;
 }
+
+// in O(n) time and O(1) space
+// nafahmidam chikar mikone :(
+
+function isPalindrome(head) {
+    if (!head || !head.next) return true; // edge cases
+    
+    // Find the middle of the linked list
+    let slow = head;
+    let fast = head;
+    while (fast.next && fast.next.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    
+    // Reverse the second half of the linked list
+    let prev = null;
+    let curr = slow.next;
+    while (curr) {
+      let temp = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = temp;
+    }
+    
+    // Compare the first half with the reversed second half
+    let p1 = head;
+    let p2 = prev;
+    while (p2) {
+      if (p1.val !== p2.val) return false;
+      p1 = p1.next;
+      p2 = p2.next;
+    }
+    
+    return true;
+  }
+  
